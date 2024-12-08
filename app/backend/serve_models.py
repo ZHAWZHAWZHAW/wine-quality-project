0d1938f1f5d3788b9abea8e253abdc68ad1aabbb
Tauli01~ <taulant.pireva@gmail.com>
2024-12-08 17:56:49 +0100

    changed models folder serving

--- app/backend/serve_models.py
+++ app/backend/serve_models.py
@@ -13,9 +13,19 @@ def load_models():
     Load all models and the scaler from the models folder and store them in the models dictionary.
     """
     global models, scaler
+    
+    # Check if the models folder exists
+    if not os.path.exists(MODELS_FOLDER):
+        print(f"Warning: The folder '{MODELS_FOLDER}' does not exist. Skipping model loading.")
+        return
+    
     try:
         # List all files in the models folder with .joblib extension
         model_files = [f for f in os.listdir(MODELS_FOLDER) if f.endswith('.joblib')]
+        
+        if not model_files:
+            print("Warning: No models found in the 'backend/models' folder.")
+            return
         
         # Load each model and add it to the models dictionary
         for model_file in model_files:
